9cf80c14e8d73eb49dba75a4d29c5c8152b8584b
Tim Gremalm <tim@gremalm.se>
2017-10-29 18:21:52 +0100

    Now self recursive object.. Scary.
    * Removed AvailableMoves, just uses ValidMoves now. Unnecesary validation step.
    * Had to fix some problems with a little too much self recursiveness...

--- KnightsInFen/KnightsInFen/Program.cs
+++ KnightsInFen/KnightsInFen/Program.cs
@@ -7,7 +7,6 @@ using System.Threading.Tasks;
 namespace KnightsInFen {
 	public class BoardMove {
 		BoardMove Parent = null;
-		public List<BoardMove> AvailableMoves = new List<BoardMove>();
 		public List<BoardMove> ValidMoves = new List<BoardMove>();
 		public List<List<char>> Board = new List<List<char>>();	//Stores the position of pieces here
 		public int Generation;
@@ -23,25 +22,32 @@ namespace KnightsInFen {
 		private const int MaxNumberOfMoves = 10;
 
 		public BoardMove (string Layout) {
-			InitBoardMove(null, 0, Layout);
+			InitBoardMove(null, 0, Layout, false);
 		}
-		public BoardMove(BoardMove parent, int generation, string Layout) {
-			InitBoardMove(parent, generation, Layout);
+		public BoardMove(BoardMove parent, int generation, string Layout, bool finished) {
+			InitBoardMove(parent, generation, Layout, finished);
 		}
-		private void InitBoardMove(BoardMove parent, int generation, string Layout) {
+		private void InitBoardMove(BoardMove parent, int generation, string Layout, bool finished) {
 			Parent = parent;
 			Generation = generation;
-			ReadLayout(Layout);
+			Board = ReadLayout(Layout);
+			Finished = finished;
+			if (Finished) {
+				SetShortestPath();
+			}
+			CalculateAvailableBoardMoves();
 		}
-		private void ReadLayout(string Layout) {
+		private List<List<char>> ReadLayout(string Layout) {
+			List<List<char>> boardOut = new List<List<char>>();
 			var lines = Layout.Split(new string[] { "\n" }, StringSplitOptions.None);
 			foreach (string line in lines) {
 				List<char> CharList = new List<char>();
 				foreach(char character in line) {
 					CharList.Add(character);
 				}
-				Board.Add(CharList);
+				boardOut.Add(CharList);
 			}
+			return boardOut;
 		}
 		private string TextRepresentationOfBoard(List<List<char>> BoardRepresent) {
 			return string.Join("\n", BoardRepresent.Select(r => String.Join("", r.Select(c => c.ToString()))));
@@ -51,10 +57,14 @@ namespace KnightsInFen {
 		}
 		private void MakeAMoveAndStore(int pieceToMoveRow, int pieceToMoveCol, int moveToRow, int moveToCol) {
 			char typeOfCharacterToMove = Board[pieceToMoveRow][pieceToMoveCol];
-			BoardMove newMove = new BoardMove(this, Generation+1, TextRepresentationOfBoard(Board));
-			newMove.Board[pieceToMoveRow][pieceToMoveCol] = " ".ToCharArray()[0];
-			newMove.Board[moveToRow][moveToCol] = typeOfCharacterToMove;
-			AvailableMoves.Add(newMove);
+
+			List<List<char>> newBoard = ReadLayout(TextRepresentationOfBoard(Board));
+			newBoard[pieceToMoveRow][pieceToMoveCol] = " ".ToCharArray()[0];
+			newBoard[moveToRow][moveToCol] = typeOfCharacterToMove;
+
+			bool isNewBoardFinished = (TextRepresentationOfBoard(newBoard).Equals(FinishedLook));
+
+			ValidMoves.Add(new BoardMove(this, Generation + 1, TextRepresentationOfBoard(newBoard), isNewBoardFinished));
 		}
 		public void CalculateAvailableBoardMoves() {
 			List<Tuple<int, int>> legalMoves = new List < Tuple < int, int>>{	new Tuple<int,int>(-2, -1),
@@ -66,6 +76,13 @@ namespace KnightsInFen {
 																				new Tuple<int,int>(2, -1),
 																				new Tuple<int,int>(2, 1),
 			};
+
+			//Check that max generation isn't reached
+			if (Generation == MaxNumberOfMoves) {
+				return;
+			}
+
+			//Todo: Check previous BoardMoves so we don't get any duplicates
 
 			//Walk through every board-position
 			for (int i=0; i<Board.Count; i++) {
@@ -94,24 +111,6 @@ namespace KnightsInFen {
 			}
 		}
 
-		public void CalculateValidBoardMoves() {
-			foreach(BoardMove move in AvailableMoves) {
-				//Todo: Check previous BoardMoves so we don't get any duplicates
-
-				//Check that max generation isn't reached
-				if (move.Generation < MaxNumberOfMoves) {
-					ValidMoves.Add(move);
-					string a = TextRepresentationOfBoard(move.Board);
-					if (TextRepresentationOfBoard(move.Board).Equals(FinishedLook)) {
-						move.Finished = true;
-					}
-					if (move.Finished) {
-						move.SetShortestPath();
-					}
-				}
-			}
-
-		}
 		private void SetShortestPath() {
 			//Walk up through parents and check if this generation is shorter then previous
 			//If shorter, set ShortestPath ItemNo and Steps for parent
@@ -131,25 +130,18 @@ namespace KnightsInFen {
 	}
 	class Program {
 		static void Main(string[] args) {
-			var a = new BoardMove(	"01011\n" +
+			var a = new BoardMove("01011\n" +
 									"110 1\n" +
 									"01110\n" +
 									"01010\n" +
 									"00100");
-			a = new BoardMove(		"11111\n" +
+			Console.WriteLine(a);
+			var b = new BoardMove(	"11111\n" +
 									"0111 \n" +
 									"00111\n" +
 									"00001\n" +
 									"00000");
-			a.CalculateAvailableBoardMoves();
-			a.CalculateValidBoardMoves();
-
-			foreach (BoardMove move in a.ValidMoves) {
-				move.CalculateAvailableBoardMoves();
-				move.CalculateValidBoardMoves();
-			}
-
-			Console.WriteLine(a);
+			Console.WriteLine(b);
 			Console.ReadLine();
 		}
 	}
